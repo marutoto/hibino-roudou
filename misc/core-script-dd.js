a6a87e01cf489bb801540a523c44fb2a8dc107b0
Yamauchi.k <yamauchi.k@dentsudigital.co.jp>
2022-09-05 16:15:38 +0900

    休暇考慮

--- misc/core-script-dd.js
+++ misc/core-script-dd.js
@@ -18,6 +18,8 @@
   }
 
   function getTimeDisplay(targetMins) {
+    // TODO: マイナスの表示オプション
+
     if (targetMins <= 0) {
       return TIME_DISPLAY_ZERO
     }
@@ -27,6 +29,8 @@
   }
 
   function output(
+    shoteiRoudouHoursPerDay,
+    kyuukaDays, // 休暇日数
     ret1, // 所定日数
     ret2, // 所定時間
     ret3, // 記入日数
@@ -42,7 +46,7 @@
 ※ 記入分(前日)までのデータで算出
 ============================
 
-# 所定 (${ SHOTEI_ROUDOU_HOURS_PER_DAY }h/Day)
+# 所定 (${ shoteiRoudouHoursPerDay }h/Day)
 - 日数: ${ ret1 }日
 - 時間: ${ ret2 }時間
 
@@ -52,9 +56,11 @@
 - 記入日数: ${ ret3 }日
 - 実労働時間: ${ ret4 }
 
+- 休暇日数: ${ kyuukaDays }日
+
 ----------------------------
 
-# 結論
+# 結論（休暇考慮済み）
 
 ## あと何日？
 - 残日数: ${ ret5 }日
@@ -81,13 +87,30 @@ ${
     )
   }
 
-  // ---
+  // --- main table
+  var mainTable = document.getElementById('mainTable')
+  var dayRows = mainTable.querySelectorAll('tr.days')
 
-  var rootEl = document.getElementById('bottomSummaryTable')
-  var _tables = rootEl.querySelectorAll('table')
+  var kyuukaDays = 0
+  dayRows.forEach((elRow) => {
+    // 休暇を取得
+    // 休暇は実働に計上されないぽいので、こっちから減らしにいく必要がある
+    //   「休暇」を含む title を7h稼働とする、でよさそう
+    //   TODO: 「午前半休」「午後半休」「時間単位有休」は、実際取得する機会があったときに仕様がわかってから別途で対応する感じでいい
+    const elStat = elRow.querySelector('.dval.vstatus')
+    const statStr = elStat.getAttribute('title') // '通常出勤日' '〇〇休暇' '午前半休' '午後半休' '時間単位有休' ...
+    if (statStr.match('休暇')) {
+      kyuukaDays++
+    }
+  })
+  var kyuukaTimesMin = retrieveTimeAsMinutes(`${ SHOTEI_ROUDOU_HOURS_PER_DAY * kyuukaDays }:00`)
+  console.log('kyuukaTimesMin', kyuukaTimesMin)
 
-  var table1 = _tables[1] // 「今月の基準日数・労働時間」
-  var table2 = _tables[2] // 「実績（勤怠入力分まで）※有休除く」
+  // --- bottom tables
+  var bottomSummaryTables = document.getElementById('bottomSummaryTable').querySelectorAll('table')
+
+  var table1 = bottomSummaryTables[1] // 「今月の基準日数・労働時間」
+  var table2 = bottomSummaryTables[2] // 「実績（勤怠入力分まで）※有休除く」
 
   var table1Rows = table1.querySelectorAll('tr')
   var table2Rows = table2.querySelectorAll('tr')
@@ -99,13 +122,17 @@ ${
 
   var shoteiDays = retrieveDay(_shoteiDays)
   var shoteiTimesMin = retrieveTimeAsMinutes(_shoteiTimes)
+  var keisokuDays = shoteiDays - kyuukaDays // 休暇分マイナス
+  var keisokuTimesMin = shoteiTimesMin - kyuukaTimesMin // 休暇分マイナス
   var jitsuDays = retrieveDay(_jitsuDays)
   var jitsuTimesMin = retrieveTimeAsMinutes(_jitsuTimes)
 
-  var zanDays = shoteiDays - jitsuDays
-  var zanTimesMin = shoteiTimesMin - jitsuTimesMin
+  var zanDays = keisokuDays - jitsuDays
+  var zanTimesMin = keisokuTimesMin - jitsuTimesMin
 
   output(
+    SHOTEI_ROUDOU_HOURS_PER_DAY,
+    kyuukaDays, // 休暇日数
     shoteiDays, // 所定日数
     shoteiTimesMin / 60, // 所定時間
     jitsuDays, // 記入日数
@@ -113,6 +140,6 @@ ${
     zanDays, // 残日数
     getTimeDisplay(zanTimesMin), // あとこれだけ働く必要がある 月末まで
     getTimeDisplay(Math.ceil(zanTimesMin / zanDays)), // あとこれだけ働く必要がある 1日平均
-    getTimeDisplay((jitsuTimesMin + zanDays * SHOTEI_ROUDOU_HOURS_PER_DAY * 60) - shoteiTimesMin), // 毎日7h働くと残業時間は
+    getTimeDisplay((jitsuTimesMin + zanDays * SHOTEI_ROUDOU_HOURS_PER_DAY * 60) - keisokuTimesMin), // 毎日7h働くと残業時間は
   )
 })()
